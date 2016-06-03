7733dde734a4c306c96dac3a690c056140cfb966
Qing Guo <qguo@maxleap.com>
2016-06-03 15:06:18 +0800

    change

--- example/android/app/src/main/java/com/demo/MainActivity.java
+++ example/android/app/src/main/java/com/demo/MainActivity.java
@@ -13,8 +13,8 @@ import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
-    private String APP_ID = "564d6a6153e70e00012cf262";
-    private String API_KEY = "UnBxUVJScDBFQ3FBZG4zaHlPenZ3UQ";
+    private String APP_ID = "574ff56a169e7d0001a56008";
+    private String API_KEY = "VHV0TlRnVzB3OFhBdDVQc0E2RnVUUQ";
 
     // private String APP_ID = "56a04b91a5ff7f0001c46a11";
     // private String API_KEY = "LXJlT2daS0tNZWxMWUFUTnh4MlBCZw";
